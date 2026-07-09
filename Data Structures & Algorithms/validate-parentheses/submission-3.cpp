78578d7ba443fce6ca44d5e8de60a0e79420913c
Dhawal Verma <101696386+dhawal-verma@users.noreply.github.com>
2026-07-09 22:44:41 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(int i=0;i<s.size();i++){
+            if(s[i]==')' || s[i]=='}' || s[i]==']'){
+                if(s[i]==')'){
+                if(!st.empty() &&  st.top()=='(')
+                    st.pop();
+                else return false;
+                }
+                else if(  s[i]=='}'){
+                if(!st.empty() && st.top()=='{')st.pop();
+                else return false;
+                }
+                else if( s[i]==']' ){
+                if(!st.empty() && st.top()=='[')
+                {st.pop();}
+                else return false;}
+            }else
+            st.push(s[i]);
+        }
+        return st.empty();
+    }
+};
