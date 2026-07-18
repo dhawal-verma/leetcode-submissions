ed5571c3a110af95a1986467cd2e5efe40118088
Dhawal Verma <101696386+dhawal-verma@users.noreply.github.com>
2026-07-18 10:48:12 +0530

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        int low  = 0;
+        int high = matrix[0].size()-1;
+
+        while(low<matrix.size() && high>=0){
+            if(matrix[low][high]==target)return true;
+            else if(matrix[low][high]>target) high--;
+            else low++;
+        }
+        return false;
+    }
+};
